adc88169758faa78d3ee14a349d5ba5f564328e6
AndriyTkh <andriytkh@icloud.com>
2025-04-06 16:56:34 +0300

    working camera
    without any cinemachine

--- Assets/Scripts/Camera/PixelPerfectFollow.cs
+++ Assets/Scripts/Camera/PixelPerfectFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PixelPerfectFollow : MonoBehaviour
@@ -15,28 +16,27 @@ public class PixelPerfectFollow : MonoBehaviour
     {
         Time.timeScale = time;
 
-        unitsPerPixel = (Camera.main.ViewportToWorldPoint(new Vector3(1f / 80f, 0, 0)) - Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0))).x;
+        // unitsPerPixel = (Camera.main.ViewportToWorldPoint(new Vector3(1f / 320f, 0, 0)) - Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0))).x;
     }
-    void Update()
+    void LateUpdate()
     {
-        if (!target) return;
+        unitsPerPixel = 1f / pixelsPerUnit;
 
-        Vector3 camPos = target.position;
+        //float offset = transform.position.x % unitsPerPixel;
 
-        float offset = unitsPerPixel != 0 ? transform.position.x % unitsPerPixel : 0;
+        transform.position = target.position + new Vector3(0, 2, -10);
 
-        transform.position = target.position + new Vector3(0, 0, -10);
 
-        // Debug.Log($"Camera {transform.position.x} offset {offset} screan {objScrean.transform.localPosition.x}");
-        // Debug.Log(Camera.allCamerasCount);
-        // Debug.Log(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1)));
-        Debug.Log(Camera.main.ViewportToWorldPoint(new Vector3(1f / 81f, 0, 1)) - Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 1)));
+        float snappedX = Mathf.Round(transform.position.x / unitsPerPixel) * unitsPerPixel;
+        float snappedY = Mathf.Round(transform.position.y / unitsPerPixel) * unitsPerPixel;
 
-        objScrean.transform.localPosition = new Vector3(offset * mul, 0, 5);
 
-        float snappedX = Mathf.Round(camPos.x / unitsPerPixel) * unitsPerPixel;
-        float snappedY = Mathf.Round(camPos.y / unitsPerPixel) * unitsPerPixel;
+        objScrean.transform.localPosition = new Vector3(snappedX - transform.position.x, snappedY - transform.position.y, 5);
 
-        pixelCamera.position = new Vector3(snappedX, snappedY, -10);
+
+        // Vector3 screanOffset = pixelCamera.position - transform.position;
+        // objScrean.transform.localPosition = new Vector3(screanOffset.x, screanOffset.y, 5);
+
+        pixelCamera.transform.position = new Vector3(snappedX, snappedY, -10);
     }
 }
